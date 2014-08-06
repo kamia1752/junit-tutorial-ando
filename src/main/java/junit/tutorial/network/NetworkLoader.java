4cdf472f90648d373fb3ed7aefd1ccc9e9afa441
Manabu Hosaka <hosaka893@gmail.com>
2014-08-06 21:15:14 +0900

    20-3まで終わったもの

---
+++ src/main/java/junit/tutorial/network/NetworkLoader.java
@@ -0,0 +1,10 @@
+package junit.tutorial.network;
+
+import java.io.InputStream;
+
+public class NetworkLoader {
+    public InputStream getInput() {
+        //TODO 未実装
+        return null;
+    }
+}
